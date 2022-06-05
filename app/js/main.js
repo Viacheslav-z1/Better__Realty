a0a9dc04171891e9a890f9e526dad9dc438ef618
Viacheslav-z1 <slavik.zaliotnyi@gmail.com>
2022-06-05 17:28:13 +0300

    added abot-page, home-li page

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,14 @@ $(function () {
   $('[data-fancybox="gallery"]').fancybox({
     // Options will go here
   });
+  $('[data-fancybox="images"]').fancybox({
+    afterLoad: function (instance, current) {
+      var pixelRatio = window.devicePixelRatio || 1;
+
+      if (pixelRatio > 1.5) {
+        current.width = current.width / pixelRatio;
+        current.height = current.height / pixelRatio;
+      }
+    }
+  });
 })
